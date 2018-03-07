0c4bcf21460553dd7d8a94f66f009918c9f4b876
Thibaud Marcé <thibaud.marce@gmail.com>
2018-03-07 22:32:28 +0100

    add photo to seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,16 +3,26 @@ Personnage.destroy_all
 
 puts 'Creating personnages'
 
-nom_personnages = %w(mickey minnie donald pluto aladin bambie)
+personnages = [
+  { nom: "mickey", photo_url: 'https://img00.deviantart.net/973f/i/2016/232/8/2/angry_mickey_mouse_by_trainboy48-daep3en.png'},
+  { nom: "donald", photo_url: 'http://moziru.com/images/donald-duck-clipart-angry-4.gif'},
+  { nom: "simba", photo_url: 'https://vignette.wikia.nocookie.net/kingdomhearts/images/9/95/Simba.png/revision/latest?cb=20120412143410&path-prefix=fr'},
+  { nom: "scar", photo_url: 'http://weclipart.com/gimg/8D0DAE9091C517F3/clipprsc.gif'},
+  { nom: "aladin", photo_url: 'https://vignette.wikia.nocookie.net/mugenpedia/images/8/8d/Aladdin.gif/revision/latest?cb=20131007165626&path-prefix=fr'},
+  { nom: "bambie", photo_url: 'https://vignette.wikia.nocookie.net/disney/images/1/13/Bambi_angry.gif/revision/latest?cb=20141214192236'},
+  ]
 
-nom_personnages.each do |n|
+
+personnages.each do |p|
   vie = (20..90).to_a.sample
   attaque = 100 - vie
-  Personnage.create!(
-    nom:              n.capitalize,
+  new_personnage = Personnage.new(
+    nom:              p[:nom],
     points_vie:       vie,
     points_attaque:   attaque,
   )
+  new_personnage.remote_photo_url = p[:photo_url]
+  new_personnage.save!
 end
 
 puts 'Finished'
