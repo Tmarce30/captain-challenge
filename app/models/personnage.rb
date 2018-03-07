d85abc72e79b0f9f7b0fee8e3d89a640aa81912b
Thibaud Marcé <thibaud.marce@gmail.com>
2018-03-07 13:16:36 +0100

    add custom validation to sum of points_attaque et points_vie

--- app/models/personnage.rb
+++ app/models/personnage.rb
@@ -2,4 +2,12 @@ class Personnage < ApplicationRecord
   validates :nom, uniqueness: true, presence: true
   validates :points_vie, uniqueness: true, presence: true
   validates :points_attaque, uniqueness: true, presence: true
+
+  validate :sum_equals_100
+
+  def sum_equals_100
+     if points_vie + points_attaque != 100
+       errors.add(:base, 'La somme des points de caractéristique doit être égale à 100')
+     end
+  end
 end
