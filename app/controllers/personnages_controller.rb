577131327654ac0aea81ccabc2e47b2448a95d92
Thibaud Marcé <thibaud.marce@gmail.com>
2018-03-07 18:27:05 +0100

    add personnage show action and view

--- app/controllers/personnages_controller.rb
+++ app/controllers/personnages_controller.rb
@@ -1,5 +1,15 @@
 class PersonnagesController < ApplicationController
+  before_action :set_personnage, only: [:show]
   def index
     @personnages = Personnage.all
+  end
+
+  def show
+  end
+
+  private
+
+  def set_personnage
+    @personnage = Personnage.find(params[:id])
   end
 end
