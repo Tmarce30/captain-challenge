589dc24f218633abc0f5caed39df7ef26f824427
Thibaud Marcé <thibaud.marce@gmail.com>
2018-03-07 13:01:43 +0100

    add validation to personnage for uniqueness and presence

--- app/models/personnage.rb
+++ app/models/personnage.rb
@@ -1,2 +1,5 @@
 class Personnage < ApplicationRecord
+  validates :nom, uniqueness: true, presence: true
+  validates :points_vie, uniqueness: true, presence: true
+  validates :points_attaque, uniqueness: true, presence: true
 end
