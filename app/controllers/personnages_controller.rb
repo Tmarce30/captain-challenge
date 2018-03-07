10a7a18c4610485e8f544321337b8f0145e4d5fa
Thibaud Marcé <thibaud.marce@gmail.com>
2018-03-07 18:45:55 +0100

    add personnage create action

--- app/controllers/personnages_controller.rb
+++ app/controllers/personnages_controller.rb
@@ -8,10 +8,23 @@ class PersonnagesController < ApplicationController
   end
 
   def new
-    @cocktail = Cocktail.new
+    @cocktail = Personnage.new
+  end
+
+  def create
+    @personnage = Personnage.new(personnage_params)
+    if @personnage.save
+      render personnage_path(@personnage)
+    else
+      render :new
+    end
   end
 
   private
+
+  def personnage_params
+    params.require(:personnage).permit(:nom, :points_vie, :points_attaque)
+  end
 
   def set_personnage
     @personnage = Personnage.find(params[:id])
