9be8fbc432d654dbf01a9c7395467dd103219a60
Thibaud Marcé <thibaud.marce@gmail.com>
2018-03-09 11:33:01 +0100

    join model personnage to joueurs and combats

--- app/models/personnage.rb
+++ app/models/personnage.rb
@@ -1,9 +1,12 @@
 class Personnage < ApplicationRecord
-  mount_uploader :photo, PhotoUploader
+
+  has_many :joueurs
+  has_many :combats, through: :joueurs
 
   validates :nom, uniqueness: true, presence: true
   validates :points_vie, presence: true
   validates :points_attaque, presence: true
+  mount_uploader :photo, PhotoUploader
 
   validate :sum_equals_100
 
