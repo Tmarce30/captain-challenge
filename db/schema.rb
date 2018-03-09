10f4e7e539cba0435824242562e3cf631403ad4a
Thibaud Marcé <thibaud.marce@gmail.com>
2018-03-09 11:13:26 +0100

    generate model combat

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180307202716) do
+ActiveRecord::Schema.define(version: 20180309101213) do
+
+  create_table "combats", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "personnages", force: :cascade do |t|
     t.string   "nom"
