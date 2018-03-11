93b86bca5e1890e0a0e734326a72a822a40a9c74
Thibaud Marcé <thibaud.marce@gmail.com>
2018-03-11 17:33:01 +0100

    refactor deroulement_combat

--- app/controllers/combats_controller.rb
+++ app/controllers/combats_controller.rb
@@ -26,58 +26,47 @@ class CombatsController < ApplicationController
   end
 
   def deroulement_combat
-    joueurs = ordre_attaque
-    @joueur_1 = joueurs[:joueur_1]
-    @joueur_2 = joueurs[:joueur_2]
-
-    @joueur_1_vie = @joueur_1.personnage.points_vie
-    @joueur_2_vie = @joueur_2.personnage.points_vie
-
-    while @joueur_1_vie > 0
-      if esquive?(@joueur_2.personnage.points_agilité)
-        @joueur_2.increment(:coups_esquivés, by = 1)
-        @joueur_2.save
-      else
-        @joueur_2_vie -= (@joueur_1.personnage.points_attaque / 10)
-        @joueur_1.increment(:coups_donnés, by = 1)
-        @joueur_1.save
-      end
-
-      break if @joueur_2_vie <= 0
-
-      if esquive?(@joueur_1.personnage.points_agilité)
-        @joueur_1.increment(:coups_esquivés, by = 1)
-        @joueur_1.save
-      else
-        @joueur_1_vie -= (@joueur_2.personnage.points_attaque / 10)
-        @joueur_2.increment(:coups_donnés, by = 1)
-        @joueur_2.save
-      end
-    end
-
+    ordre_attaque
+    rounds
     resultat
-  end
-
-  def ordre_attaque
-    joueurs = @combat.joueurs
-    joueur_1 = joueurs.sample
-    joueur_2 = joueurs.reject { |joueur| joueur == joueur_1 }
-    return { joueur_1: joueur_1, joueur_2: joueur_2.first }
-  end
-
-  def resultat
-    if @joueur_1_vie <= 0
-      @joueur_1.resultat = 'Perdant'
-      @joueur_2.resultat = 'Vainqueur'
-    elsif @joueur_2_vie <= 0
-      @joueur_1.resultat = 'Vainqueur'
-      @joueur_2.resultat = 'Perdant'
-    end
     @joueur_1.save
     @joueur_2.save
   end
 
+  def ordre_attaque
+    joueurs = @combat.joueurs
+    @joueur_1 = joueurs.sample
+    @joueur_2 = joueurs.reject { |joueur| joueur == @joueur_1 }.first
+  end
+
+  def rounds
+    while @joueur_1.personnage.points_vie > 0
+      coups(@joueur_1, @joueur_2)
+      break if @joueur_2.personnage.points_vie <= 0
+      coups(@joueur_2, @joueur_1)
+    end
+  end
+
+  def coups(joueur_qui_attaque, joueur_qui_défend)
+    if esquive?(joueur_qui_défend.personnage.points_agilité)
+      joueur_qui_défend.increment(:coups_esquivés, by = 1)
+    else
+      joueur_qui_défend.personnage.points_vie -= (joueur_qui_attaque.personnage.points_attaque / 10)
+      joueur_qui_attaque.increment(:coups_donnés, by = 1)
+    end
+  end
+
   def esquive?(joueur_agilite)
     rand <= joueur_agilite / 100.0
+  end
+
+  def resultat
+    if @joueur_1.personnage.points_vie <= 0
+      @joueur_1.resultat = 'Perdant'
+      @joueur_2.resultat = 'Vainqueur'
+    elsif @joueur_2.personnage.points_vie <= 0
+      @joueur_1.resultat = 'Vainqueur'
+      @joueur_2.resultat = 'Perdant'
+    end
   end
 end
