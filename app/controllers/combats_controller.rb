a80d08493fae2e97fd3e9687388edfdbd0dabf8c
Thibaud Marcé <thibaud.marce@gmail.com>
2018-03-11 13:20:11 +0100

    add method deroulement_combat to create combat that makes a simple fight between two players with a random first attacker

--- app/controllers/combats_controller.rb
+++ app/controllers/combats_controller.rb
@@ -12,6 +12,7 @@ class CombatsController < ApplicationController
   def create
     @combat = Combat.new(combat_params)
     if @combat.save
+      deroulement_combat(@combat)
       redirect_to combat_path(@combat)
     else
       render :new
@@ -22,5 +23,25 @@ class CombatsController < ApplicationController
 
   def combat_params
     params.require(:combat).permit(joueurs_attributes: [:id, :personnage_id])
+  end
+
+  def deroulement_combat(combat)
+    joueurs = ordre_attaque(combat)
+
+    joueur_1_vie = joueurs[:joueur_1].personnage.points_vie
+    joueur_2_vie = joueurs[:joueur_2].personnage.points_vie
+
+    while joueur_1_vie > 0
+      joueur_2_vie -= (joueurs[:joueur_1].personnage.points_attaque / 10)
+      break if joueur_2_vie <= 0
+      joueur_1_vie -= (joueurs[:joueur_2].personnage.points_attaque / 10)
+    end
+  end
+
+  def ordre_attaque(combat)
+    joueurs = combat.joueurs
+    joueur_1 = joueurs.sample
+    joueur_2 = joueurs.reject { |joueur| joueur == joueur_1 }
+    return { joueur_1: joueur_1, joueur_2: joueur_2.first }
   end
 end
