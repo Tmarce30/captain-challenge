f94e1f4abe3ae5e1e5f44a3fcb7d5d148275b43b
Thibaud Marcé <thibaud.marce@gmail.com>
2018-03-11 17:01:23 +0100

    refactor with instance variables

--- app/controllers/combats_controller.rb
+++ app/controllers/combats_controller.rb
@@ -12,7 +12,7 @@ class CombatsController < ApplicationController
   def create
     @combat = Combat.new(combat_params)
     if @combat.save
-      deroulement_combat(@combat)
+      deroulement_combat
       redirect_to combat_path(@combat)
     else
       render :new
@@ -25,54 +25,56 @@ class CombatsController < ApplicationController
     params.require(:combat).permit(joueurs_attributes: [:id, :personnage_id])
   end
 
-  def deroulement_combat(combat)
-    joueurs = ordre_attaque(combat)
+  def deroulement_combat
+    joueurs = ordre_attaque
+    @joueur_1 = joueurs[:joueur_1]
+    @joueur_2 = joueurs[:joueur_2]
 
-    joueur_1_vie = joueurs[:joueur_1].personnage.points_vie
-    joueur_2_vie = joueurs[:joueur_2].personnage.points_vie
+    @joueur_1_vie = @joueur_1.personnage.points_vie
+    @joueur_2_vie = @joueur_2.personnage.points_vie
 
-    while joueur_1_vie > 0
-      if esquive?(joueurs[:joueur_2].personnage.points_agilité)
-        joueurs[:joueur_2].increment(:coups_esquivés, by = 1)
-        joueurs[:joueur_2].save
+    while @joueur_1_vie > 0
+      if esquive?(@joueur_2.personnage.points_agilité)
+        @joueur_2.increment(:coups_esquivés, by = 1)
+        @joueur_2.save
       else
-        joueur_2_vie -= (joueurs[:joueur_1].personnage.points_attaque / 10)
-        joueurs[:joueur_1].increment(:coups_donnés, by = 1)
-        joueurs[:joueur_1].save
+        @joueur_2_vie -= (@joueur_1.personnage.points_attaque / 10)
+        @joueur_1.increment(:coups_donnés, by = 1)
+        @joueur_1.save
       end
 
-      break if joueur_2_vie <= 0
+      break if @joueur_2_vie <= 0
 
-      if esquive?(joueurs[:joueur_1].personnage.points_agilité)
-        joueurs[:joueur_1].increment(:coups_esquivés, by = 1)
-        joueurs[:joueur_1].save
+      if esquive?(@joueur_1.personnage.points_agilité)
+        @joueur_1.increment(:coups_esquivés, by = 1)
+        @joueur_1.save
       else
-        joueur_1_vie -= (joueurs[:joueur_2].personnage.points_attaque / 10)
-        joueurs[:joueur_2].increment(:coups_donnés, by = 1)
-        joueurs[:joueur_2].save
+        @joueur_1_vie -= (@joueur_2.personnage.points_attaque / 10)
+        @joueur_2.increment(:coups_donnés, by = 1)
+        @joueur_2.save
       end
     end
 
-    resultat(joueurs, joueur_1_vie, joueur_2_vie)
+    resultat
   end
 
-  def ordre_attaque(combat)
-    joueurs = combat.joueurs
+  def ordre_attaque
+    joueurs = @combat.joueurs
     joueur_1 = joueurs.sample
     joueur_2 = joueurs.reject { |joueur| joueur == joueur_1 }
     return { joueur_1: joueur_1, joueur_2: joueur_2.first }
   end
 
-  def resultat(joueurs, joueur_1_vie, joueur_2_vie)
-    if joueur_1_vie <= 0
-      joueurs[:joueur_1].resultat = 'Perdant'
-      joueurs[:joueur_2].resultat = 'Vainqueur'
-    elsif joueur_2_vie <= 0
-      joueurs[:joueur_1].resultat = 'Vainqueur'
-      joueurs[:joueur_2].resultat = 'Perdant'
+  def resultat
+    if @joueur_1_vie <= 0
+      @joueur_1.resultat = 'Perdant'
+      @joueur_2.resultat = 'Vainqueur'
+    elsif @joueur_2_vie <= 0
+      @joueur_1.resultat = 'Vainqueur'
+      @joueur_2.resultat = 'Perdant'
     end
-    joueurs[:joueur_1].save
-    joueurs[:joueur_2].save
+    @joueur_1.save
+    @joueur_2.save
   end
 
   def esquive?(joueur_agilite)
