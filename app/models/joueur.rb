f426f7947e87e9766892366fa9c0f9131a07bcb2
Thibaud Marcé <thibaud.marce@gmail.com>
2018-03-12 00:21:30 +0100

    add validation to joueur

--- app/models/joueur.rb
+++ app/models/joueur.rb
@@ -3,4 +3,8 @@ class Joueur < ApplicationRecord
   belongs_to :combat, required: false
   belongs_to :bouclier
   belongs_to :arme
+
+  validate :personnage_id, presence: true
+  validate :bouclier_id, presence: true
+  validate :arme_id, presence: true
 end
